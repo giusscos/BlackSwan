e95c3b94e1620352d1418e71f520d5e645b65893
giusscos <giu.cosenza08@gmail.com>
2025-04-11 22:35:26 +0200

    add: edit mode and function to save changes

--- BlackSwan/View/SwanDetailsView.swift
+++ BlackSwan/View/SwanDetailsView.swift
@@ -6,38 +6,117 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SwanDetailsView: View {
-    var swan: Swan = Swan(title: "Test 1", text: "Lorem ipsum")
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
+    
+    var swan: Swan
+    @State private var isEditing = false
+    @State private var editedTitle: String
+    @State private var editedText: String
+    
+    init(swan: Swan) {
+        self.swan = swan
+        _editedTitle = State(initialValue: swan.title)
+        _editedText = State(initialValue: swan.text)
+    }
     
     var body: some View {
         VStack (alignment: .leading) {
-            Text(Date(), format: Date.FormatStyle(date: .abbreviated, time: .shortened))
+            HStack {
+                if isEditing {
+                    Button(role: .destructive) {
+                        modelContext.delete(swan)
+                        dismiss()
+                    } label: {
+                        Label("Delete", systemImage: "trash")
+                            .labelStyle(.titleOnly)
+                    }
+                    .padding(.vertical, 8)
+                    .padding(.horizontal)
+                    .fontWeight(.semibold)
+                    .background(.red)
+                    .foregroundStyle(.white)
+                    .clipShape(Capsule())
+                } else {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Label("Back", systemImage: "chevron.left")
+                    }
+                    .padding(.vertical, 8)
+                    .padding(.horizontal)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.background)
+                    .background(.primary)
+                    .clipShape(Capsule())
+                }
+                
+                Group {
+                    if isEditing {
+                        Button("Done") {
+                            saveChanges()
+                            withAnimation {
+                                isEditing = false
+                            }
+                        }
+                    } else {
+                        Button("Edit") {
+                            withAnimation {
+                                isEditing = true
+                            }
+                        }
+                    }
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                .fontWeight(.semibold)
+                .foregroundStyle(.background)
+                .background(.primary)
+                .clipShape(Capsule())
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            .padding(.vertical)
+            
+            Text(swan.timestamp, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
                 .font(.caption)
                 .fontWeight(.semibold)
                 .foregroundStyle(.secondary)
             
-            Text(swan.title)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            Text(swan.text)
-                .font(.headline)
+            if isEditing {
+                TextField("Title", text: $editedTitle)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                TextEditor(text: $editedText)
+                    .font(.headline)
+                    .textEditorStyle(.plain)
+                
+            } else {
+                Text(swan.title)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text(swan.text)
+                    .font(.headline)
+            }
         }
         .navigationBarBackButtonHidden()
         .foregroundStyle(.white)
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .background(RadialGradient(colors: [.secondary, .primary], center: .topLeading, startRadius: 10, endRadius: 700))
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                EditButton()
-                    .tint(.white)
-            }
-        }
+    }
+    
+    private func saveChanges() {
+        swan.title = editedTitle
+        swan.text = editedText
+        swan.timestamp = Date()
     }
 }
 
 #Preview {
-    SwanDetailsView()
+    SwanDetailsView(swan: Swan(title: "Test 1", text: "Lorem ipsum"))
 }
